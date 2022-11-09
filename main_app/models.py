bbd467b5ad617f21bfab809e175b2657704ebc66
Luigi T <luigitejada@yahoo.com>
2022-11-08 23:19:09 -0500

    implement add toy to dog

--- main_app/models.py
+++ main_app/models.py
@@ -9,12 +9,23 @@ MEALS = (
   ('S', 'Snack'),
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+      
+  def get_absolute_url(self):
+    return reverse("toys_detail", kwargs={"pk": self.id})
+    
 # Create your models here.
 class Dog(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
@@ -42,13 +53,4 @@ class Feeding(models.Model):
   class Meta:
     ordering = ['-date']
 
-class Toy(models.Model):
-  name = models.CharField(max_length=50)
-  color = models.CharField(max_length=20)
-
-  def __str__(self):
-    return self.name
-      
-  def get_absolute_url(self):
-      return reverse("toys_detail", kwargs={"pk": self.id})
   
