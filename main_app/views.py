0e7dadb5ecf458374ba5a57482e3fbf681387ce6
Luigi T <luigitejada@yahoo.com>
2022-11-08 22:17:18 -0500

    implement edit functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here.
@@ -11,12 +11,20 @@ def about(request):
 
 def dogs_index(request):
   dogs = Dog.objects.all()
-  return render(request, 'dogs/index.html', {'dogs':dogs})
+  return render(request, 'dogs/index.html', {'dogs' : dogs})
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', {'dog':dog})
+  return render(request, 'dogs/detail.html', {'dog' : dog})
 
 class DogCreate(CreateView):
-  model= Dog
+  model = Dog
   fields = '__all__'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
